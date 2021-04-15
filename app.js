c640943ced939cbf21bcb2bda1c852f376302179
kc emmanuel <fidekg123@gmail.com>
2021-04-15 19:05:17 +0100

    'graphql_server1'

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
-const express = require('express');
+const express = require('express')
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema')
-const mongoose = require('mongoose');
+const schema = require('./schema/schema');
+ const mongoose = require('mongoose');
 
+ mongoose.connect(
+ 'mongodb://localhost/grl_server'
+ , {
+   useNewUrlParser: true,
+   useCreateIndex: true,
+   useUnifiedTopology: true
+ });
 
-const app = express();
+ mongoose.connection.once('open', () => {
+     console.log('connected to db')
+ })
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+   graphiql: true
+}))
+
+ 
+app.listen(4000, () => {
+    console.log(`Server started on port 4000`);
+});
+
 
 /* mongoose.connect('mongodb+srv://kcpele:kcpele123@cluster0.v2lru.mongodb.net/test',
 { useNewUrlParser: true,
     useUnifiedTopology: true} ) 
     */
-mongoose.connect('mongodb://localhost/grl_server',
-{ useNewUrlParser: true,
-    useUnifiedTopology: true})
 
-mongoose.connection.once('open', () => {
-    console.log("connected to database")
-})
-
-app.use('/graphql',graphqlHTTP({ 
-    schema,
-    graphiql: true
-}))
-
-app.listen(4000, () => {
-    console.log(`Server started on port 4000`);
-});
--- models/author.js
+++ models/author.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-
-const authorSchema = new Schema({
+const authorSchema = new mongoose.Schema({
     name: String,
     age: Number
-});
+})
 
-module.exports = mongoose.model('Author', authorSchema);
+module.exports = mongoose.model('Author', authorSchema)
